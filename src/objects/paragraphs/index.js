da86867e13e684d36ab67708462322cc2d422bdd
Fatjon Rrapaj <--global>
2022-02-16 09:28:26 +0100

    Updates

--- src/objects/paragraphs/index.js
+++
@@ -1,7 +0,0 @@
-import Time from "./Time";
-import Patience from "./Patience";
-import Android from "./Android";
-import IOS from "./IOS";
-import ReactJS from "./ReactJS";
-
-export { Time, Patience, Android, IOS, ReactJS };
